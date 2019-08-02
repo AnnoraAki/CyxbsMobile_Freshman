473db1c3359d37874c40f464b733412c7fc2bf43
yuanbing <2564800726@qq.com>
2019-08-02 18:15:33 +0800

    数据揭秘

--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/view/activity/CampusGuidelinesActivity.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/view/activity/CampusGuidelinesActivity.kt
@@ -1,18 +1,19 @@
 package com.mredrock.cyxbs.freshman.view.activity
 
-import android.content.Intent
 import android.os.Bundle
 import com.mredrock.cyxbs.common.ui.BaseActivity
 import com.mredrock.cyxbs.freshman.R
 
-class MainActivity : BaseActivity() {
+/**
+ * Create by yuanbing
+ * on 2019/8/2
+ */
+class CampusGuidelinesActivity : BaseActivity() {
     override val isFragmentActivity: Boolean
-        get() = false
+        get() = true
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.freshman_activity_main)
-        val intent = Intent(this, CampusGuidelinesActivity::class.java)
-        startActivity(intent)
+        setContentView(R.layout.freshman_activity_campus_guidelines)
     }
 }
--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/view/widget/Placeholder.kt
+++
@@ -1,6 +0,0 @@
-package com.mredrock.cyxbs.freshman.view.widget
-
-/**
- * Create by yuanbing
- * on 2019/8/1
- */
