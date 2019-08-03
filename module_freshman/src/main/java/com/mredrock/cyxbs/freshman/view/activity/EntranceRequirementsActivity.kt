553bf25b524e21785e8a2215866defe9d2c8b32d
yuanbing <2564800726@qq.com>
2019-08-03 10:22:08 +0800

    创建activity

---
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/view/activity/EntranceRequirementsActivity.kt
@@ -0,0 +1,12 @@
+package com.mredrock.cyxbs.freshman.view.activity
+
+import com.mredrock.cyxbs.common.ui.BaseActivity
+
+/**
+ * Create by yuanbing
+ * on 2019/8/3
+ */
+class EntranceRequirementsActivity : BaseActivity() {
+    override val isFragmentActivity: Boolean
+        get() = false
+}
