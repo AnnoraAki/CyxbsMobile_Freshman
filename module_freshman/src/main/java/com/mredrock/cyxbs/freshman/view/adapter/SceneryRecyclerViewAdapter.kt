814897025203d30d344e9baec502655363c6fd09
roger <jjtom5f@gmail.com>
2019-08-13 00:28:04 +0800

    字体加粗

--- module_freshman/src/main/java/com/mredrock/cyxbs/freshman/view/adapter/SceneryRecyclerViewAdapter.kt
+++ module_freshman/src/main/java/com/mredrock/cyxbs/freshman/view/adapter/SceneryRecyclerViewAdapter.kt
@@ -1,6 +1,10 @@
 package com.mredrock.cyxbs.freshman.view.adapter
 
 import android.content.Context
+import android.graphics.Typeface
+import android.text.Spannable
+import android.text.SpannableString
+import android.text.style.StyleSpan
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
@@ -9,6 +13,7 @@ import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.bumptech.glide.Glide
 import com.bumptech.glide.request.RequestOptions
+import com.mredrock.cyxbs.common.BaseApp
 import com.mredrock.cyxbs.common.utils.LogUtils
 import com.mredrock.cyxbs.common.utils.extensions.setImageFromUrl
 import com.mredrock.cyxbs.freshman.R
@@ -52,19 +57,21 @@ class SceneryRecyclerViewAdapter(val ctx: Context, val list: List<Photo>) : Recy
     }
 
     override fun onBindViewHolder(holder: RecyclerView.ViewHolder, position: Int) {
-        val options = RequestOptions().centerCrop()
         if (holder is CommonViewHolder) {
             holder.title.text = list.get(position).name
-//            Glide.with(ctx).load(list[position].photo).apply(options).into(holder.photo)
             holder.photo.setImageFromUrl(list[position].photo)
             holder.itemView.setOnClickListener {
 
                 mOnItemClickListener?.onItemClick(position)
             }
         } else if (holder is HeaderViewHolder) {
-            holder.title.text = list.get(position).name
-//            Glide.with(ctx).load(list[position].photo).apply(options).into(holder.photo)
+            holder.title.text = list[position].name
             holder.photo.setImageFromUrl(list[position].photo)
+            val styleSpan = StyleSpan(Typeface.BOLD)
+            val sb = SpannableString(BaseApp.context.getString(R.string.freshman_scenery_big))
+            sb.setSpan(styleSpan, 17, 21, Spannable.SPAN_INCLUSIVE_INCLUSIVE)
+            holder.tvBold.text = sb
+
             holder.itemView.setOnClickListener {
 
                 mOnItemClickListener?.onItemClick(position)
@@ -82,5 +89,6 @@ class SceneryRecyclerViewAdapter(val ctx: Context, val list: List<Photo>) : Recy
     class HeaderViewHolder(view: View) : RecyclerView.ViewHolder(view) {
         val title: TextView = view.find(R.id.tv_scenery_recycle_item_header)
         val photo: ImageView = view.find(R.id.iv_scenery_recycle_item_header)
+        val tvBold: TextView = view.find(R.id.tv_scenery_header_content)
     }
 }
